2de9997999abf5341ea018dde6ba77e974e1b2e1
Nicholas <nicho.tjornelund@gmail.com>
2022-12-01 20:34:03 +0100

    first blood

---
+++ 2022/AoC/Program.cs
@@ -0,0 +1,29 @@
+﻿/*string? line = Console.ReadLine();
+
+
+var max = 0;
+var current = 0;
+while (line != null)
+{
+    
+}*/
+
+using AoC;
+
+//pt1
+/*Utils.GetFileLines("input1.txt")
+    .ToList()
+    .Partition(x => x == "")
+    .Select(x => x.Select(int.Parse).Sum())
+    .Max()
+    .Print();*/
+    
+//pt2
+Utils.GetFileLines("input1.txt")
+    .ToList()
+    .Partition(x => x == "")
+    .Select(x => x.Select(int.Parse).Sum())
+    .OrderByDescending(x=>x)
+    .Take(3)
+    .Sum()
+    .Print();
---
+++ 2022/AoC/Utils.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC;
+
+public static class Utils
+{
+    public static IEnumerable<string> GetConsoleLines()
+    {
+        string line = Console.ReadLine();
+        while (( line = Console.ReadLine()) != "x") 
+            yield return line;
+    }
+    
+    public static IEnumerable<string> GetFileLines(string file)
+    {
+        return File.ReadLines(Path.Combine("../../../",file));
+    }
+    
+    public static IEnumerable<IEnumerable<T>> Partition<T>(this IList<T> source, Func<T,bool> pred)
+    {
+        var i = 0;
+        var j = 0;
+        foreach (var x in source)
+        {
+            if (pred(x))
+            {
+                yield return source.Take(new Range(i, j));
+                i = j+1;
+            }
+
+            j++;
+        }
+    }
+    
+
+    /*public static IEnumerable<IEnumerable<string>> RegexMatch(this string input,string pattern)
+    {
+        var regex = new Regex(pattern);
+        var matches = regex.Matches(input);
+        return matches.Select(x => x.Groups.Values.Select(y => y.Value));
+    }
+
+    private static string PatternMatch(string raw)
+    {
+        return raw.Replace(@"", "");
+
+    }
+    
+    
+
+    public static IEnumerable<IEnumerable<string>> GetRegexConsoleLines(string pattern)
+    {
+        return string.Join("", GetConsoleLines()).RegexMatch(pattern);
+    }
+    */
+
+    public static string ToStringg(this IEnumerable<string> list, string sep = ",")
+    {
+        return string.Join(sep, list);
+    }
+    
+    public static void Print(this IEnumerable<string> list)
+    {
+        Console.WriteLine(list.ToStringg());
+    }
+    public static void Print(this object o)
+    {
+        Console.WriteLine(o);
+    }
+}
