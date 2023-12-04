20df143b7f3aed99499d93363c8dd109f6a0e000
Nicholas <nicho.tjornelund@gmail.com>
2023-12-04 21:33:12 +0100

    day3

--- 2023/AoC/AoC/day1.fs
+++ 2023/AoC/AoC/day1.fs
@@ -4,43 +4,41 @@
 open System.IO
 open System.Collections.Generic
 open System
+open System.Text.RegularExpressions
     
 //let lines = File.ReadAllLines("input.txt")
 //let (|Int|_|) (s: string) = try Some (int s) with | _ -> None
 
+let theNumbers = "one|two|three|four|five|six|seven|eight|nine"
+let regex = Regex($"(?=\d|{theNumbers})(\d|{theNumbers})")
 
-let numberWords = ["zero";"one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"]
-let replacements = ["z1o";"o1e"; "t2o"; "t3e"; "f4r"; "f5e"; "s6x"; "s7n"; "e8t"; "n9e"]
+let nums = 
+    seq {
+        ("one",1);
+        ("two",2);
+        ("three",3);
+        ("four",4);
+        ("five",5);
+        ("six",6);
+        ("seven",7);
+        ("eight",8);
+        ("nine",9);
+    } |> dict |> Dictionary
 
-let words = 
-    Seq.zip numberWords replacements 
-    |> Seq.map (fun (k,v) -> KeyValuePair(k,v))
-    |> Dictionary<string, string>
-
-
-let rec replaceWord (s:string) (word:string) = 
-    match s.IndexOf(word) with
-    | -1 -> s
-    | i -> replaceWord $"{s[..i-1]}{words[word]}{s[i+word.Length..]}" word
-
-let replaceWords (s:string) = 
-    words.Keys
-    |> Seq.fold (fun updated key -> replaceWord updated key) s
-
-let firstlast array = $"{Seq.find Char.IsDigit array}{Seq.findBack Char.IsDigit array}" |> int
-
-let solveLine s = 
-    let a = replaceWords s
-    let b = firstlast (a.ToCharArray())
-    b |> int
-
+let find s =
+    let matchh = regex.Matches(s)
+    let first = matchh.Item(0).Value
+    let last = matchh.Item(matchh.Count-1).Value
+    let f = if nums.ContainsKey first then nums[first] else int first
+    let l = if nums.ContainsKey last then nums[last] else int last
+    $"{f}{l}" |> int
 
 let sum () = 
     let lines = File.ReadAllLines("input.txt")
     lines 
-    |> Seq.map solveLine
+    |> Seq.map find
     |> Seq.sum
     
-let t = solveLine "11four11"
+let t = find "twoneightwo123eighthreeight"
 printfn $"{t}"
 printfn $"{sum()}" 
---
+++ 2023/AoC/AoC/day3.fs
@@ -0,0 +1,80 @@
+﻿module day3
+
+open System.IO
+open System;
+open System.Collections.Generic
+
+let grid = File.ReadAllLines("day3.txt")
+        |> Seq.map _.ToCharArray()
+        |> Array.ofSeq
+
+let n = grid[0].Length
+let m = grid.Length
+
+let relativePositions =
+        [ (-1, -1); (-1, 0); (-1, 1)
+          (0, -1);           (0, 1)
+          (1, -1);  (1, 0);  (1, 1) ]
+
+let rec FindDigitSequence (i,a,b) =
+    if a-1>=0 && Char.IsDigit grid[i].[a-1] then FindDigitSequence(i, a-1, b)
+    else if b+1 < m && Char.IsDigit grid[i].[b+1] then FindDigitSequence(i, a, b+1)
+    else (a,b)
+
+let RecoverNumber (globalVisited:HashSet<int*int>) (i,j) = 
+    if globalVisited.Contains (i,j) then 
+        0
+    else
+        let a,b = FindDigitSequence(i, j,j) 
+        let visited = seq {a .. b} |> Seq.map (fun j -> (i,j)) |> HashSet
+        globalVisited.UnionWith visited
+        new string(grid[i].[a..b]) |> int
+
+let pt1 () =
+    let globalVisited = new HashSet<int*int>()
+    let VisiteSymbol (i,j) = relativePositions 
+                            |> Seq.map (fun (a,b) -> (a+i,b+j))
+                            |> Seq.filter (fun (a,b) -> Char.IsDigit grid[a].[b])
+                            |> Seq.map (fun pos -> RecoverNumber globalVisited pos) 
+                            |> Seq.sum
+
+    let Solve () = 
+        let mutable sum = 0
+        for i in 0 .. n - 1  do    
+            for j in 0 .. m - 1 do
+                if not (Char.IsDigit grid[i].[j]) && grid[i].[j] <> '.' then
+                    sum <- sum + VisiteSymbol (i,j)      
+        sum
+
+    Solve ()
+    
+
+
+let pt2 () =
+    let globalVisited = new HashSet<int*int>()
+
+    let VisiteGear (i,j) = 
+        let numbers = relativePositions 
+                    |> Seq.map (fun (a,b) -> (a+i,b+j))
+                    |> Seq.filter (fun (a,b) -> Char.IsDigit grid[a].[b])
+                    |> Seq.map (fun pos -> RecoverNumber globalVisited pos) 
+                    |> Seq.filter (fun x -> x <> 0)
+                    |> List.ofSeq
+
+        if numbers.Length = 2 then
+            numbers[0] * numbers[1]
+        else 
+            0
+
+    
+    let Solve () = 
+        let mutable sum = 0
+        for i in 0 .. n - 1  do    
+            for j in 0 .. m - 1 do
+                if grid[i].[j] = '*' then
+                    sum <- sum + VisiteGear (i,j)      
+        sum
+
+    Solve ()
+    
+printfn $"{pt2()}"
