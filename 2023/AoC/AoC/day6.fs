e88c916da012cbf0fab57dcb8e03b06fe5a26bf7
Nicholas <nicho.tjornelund@gmail.com>
2023-12-06 21:08:18 +0100

    Update day6.fs

--- 2023/AoC/AoC/day6.fs
+++ 2023/AoC/AoC/day6.fs
@@ -3,52 +3,30 @@
 open System.Text.RegularExpressions
 open System.IO
 
+let parse regexExtractor = 
+    let lines = File.ReadAllLines("day6.txt") |> List.ofSeq
+    let times = regexExtractor lines[0]
+    let distances = regexExtractor lines[1]
+    times, distances
 
+let distance t hold = (t-hold) * hold
 
-
+let howManyWays (t, record) = 
+    seq {0L .. t} 
+    |> Seq.map (fun hold -> distance t hold) 
+    |> Seq.filter (fun dist -> dist>record)
+    |> Seq.length
 
 let pt1() = 
-    let extractIntegers (input: string) = Regex("\d+").Matches(input) |> Seq.map (fun m -> int m.Value)
-
-    let parse () = 
-        let lines = File.ReadAllLines("day6.txt") |> List.ofSeq
-        let times = extractIntegers lines[0]
-        let distances = extractIntegers lines[1]
-        times, distances
-
-    let distance t hold = (t-hold) * hold
-
-    let howManyWays (t, record) = 
-        seq {0 .. t} 
-        |> Seq.map (fun hold -> distance t hold) 
-        |> Seq.filter (fun dist -> dist>record)
-        |> Seq.length
-
-    let times, distances = parse()
+    let extractIntegers (input: string) = Regex("\d+").Matches(input) |> Seq.map (fun m -> int64 m.Value)
+    let times, distances = parse extractIntegers
     let races = Seq.zip times distances
     let answer = races |> Seq.map howManyWays |> Seq.reduce (*)
     printfn $"{answer}"
 
 let pt2() = 
     let extractOneIntegers (input: string) = Regex("\d+").Matches(input) |> Seq.map _.Value |> Seq.reduce (+) |> int64
-
-    let parse () = 
-        let lines = File.ReadAllLines("day6.txt") |> List.ofSeq
-        let time = extractOneIntegers lines[0]
-        let distance = extractOneIntegers lines[1]
-        time, distance
-
-    let distance t hold = (t-hold) * hold
-
-    let howManyWays (t, record) = 
-        seq {0L .. t} 
-        |> Seq.map (fun hold -> distance t hold) 
-        |> Seq.filter (fun dist -> dist>record)
-        |> Seq.length
-
-
-    let time, distance = parse ()
-
-    let answer = howManyWays (time,distance)
-    printfn $"{answer}"
+    let time, distance = parse extractOneIntegers
+    printfn $"{howManyWays (time,distance)}"
+pt1()
 pt2()
