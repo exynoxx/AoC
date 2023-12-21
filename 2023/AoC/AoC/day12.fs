4e1d25967af99d59f68a6e7ea2af245af29305fd
Nicholas <nicho.tjornelund@gmail.com>
2023-12-21 17:28:35 +0100

    day 12 pt2

--- 2023/AoC/AoC/day12.fs
+++ 2023/AoC/AoC/day12.fs
@@ -3,58 +3,61 @@
 open System.Collections.Generic
 open System.IO
 
-let rec arrangments (row:char array) (nums:int array) i hashs j (acc:string) = 
-    if acc.StartsWith ".###.##." then
-        ()
-    let dotCase () = 
-        match hashs with 
-        | 0 -> memoArrangments row nums (i+1) 0 j (acc + ".")
-        | x when x = nums[j] -> memoArrangments row nums (i+1) 0 (j+1) (acc + ".")
-        | _ -> 0 //maybe rm
+let rec arrangments (dict:Dictionary<int*int*int,int64>) (row:char array) (nums:int array) i hashs j = 
 
-    let hashCase () = memoArrangments row nums (i+1) (hashs+1) j (acc + "#")
+    let dot () = 
+        match hashs with 
+        | 0 -> memoArrangments dict row nums (i+1) 0 j
+        | x when x = nums[j] -> memoArrangments dict row nums (i+1) 0 (j+1)
+        | _ -> 0L
+
+    let hash () = memoArrangments dict row nums (i+1) (hashs+1) j
                        
     if i >= row.Length then 
         //finish block
-        if j = nums.Length - 1 && hashs = nums[j] then
-            1
-        elif j = nums.Length then 
-            1 
+        if j = nums.Length - 1 && hashs = nums[j] || j = nums.Length then
+            1L
         else 
-            0
+            0L
     else
         match row[i] with
-        | '#' when j = nums.Length -> 0
-        | _ when j = nums.Length -> dotCase()
-        | '.' -> dotCase()
-        | '#' -> hashCase()
+        | '#' when j = nums.Length -> 0L
+        | _ when j = nums.Length -> dot()
+        | '.' -> dot()
+        | '#' -> hash()
         | '?' -> 
             match hashs with
-            | x when x = nums[j] -> dotCase()
-            | x when x > nums[j] -> 0
-            | _ -> 
-                let l = hashCase() 
-                let r = dotCase()
-                l+r
+            | x when x = nums[j] -> dot()
+            | _ -> hash() + dot()
 
-and memoArrangments (row:char array) (nums:int array) i hashs j acc =
-    arrangments row nums i hashs j acc
+and memoArrangments dict row nums i hashs j =
+    match dict.TryGetValue ((i,hashs,j)) with
+    | (true, x) -> x
+    | _ -> 
+        let value = arrangments dict row nums i hashs j
+        dict.Add ((i,hashs,j), value)
+        value
 
-let niceArrangments (row, nums) = memoArrangments row nums 0 0 0 ""
-
-let parse (s:string) = 
-    let [|left;right|] = s.Split (' ')
-    let nums = right.Split(',') |> Array.map int
-    (left.ToCharArray(),nums)
+let niceArrangments (row, nums) = memoArrangments (new Dictionary<int*int*int,int64>()) row nums 0 0 0
 
 let pt1() = 
-    let list = File.ReadAllLines("day12.txt") |> Seq.map parse |> Seq.map niceArrangments |> List.ofSeq
-    let sum = list |> List.sum
-    let out = list |> List.map string |> String.concat ","
-    printfn $"{out} {sum}"
+    let parse (s:string) = 
+        let [|left;right|] = s.Split (' ')
+        let nums = right.Split(',') |> Array.map int
+        (left.ToCharArray(),nums)
+
+    let sum = File.ReadAllLines("day12.txt") |> Seq.map parse |> Seq.map niceArrangments |> Seq.sum
+    printfn $"{sum}"
+
+let pt2() = 
+    let parse (s:string) = 
+        let [|left;right|] = s.Split (' ')
+        let springs = left |> List.replicate 5 |> String.concat "?" |> _.ToCharArray()
+        let nums = right.Split(',') |> Seq.map int |> Array.ofSeq |> Array.replicate 5 |> Array.ofSeq |> Array.collect (fun x -> x)
+        (springs,nums)
+
+    let sum = File.ReadAllLines("day12.txt") |> Seq.map parse |> Seq.map niceArrangments |> Seq.sum
+    printfn $"{sum}"
 
 pt1()
-
-(*let list = ["..?#??#???#?????. 5,6"] |> Seq.map parse |> Seq.map niceArrangments |> List.ofSeq
-let sum = list |> List.sum
-printfn $"{sum}"*)
+pt2()
