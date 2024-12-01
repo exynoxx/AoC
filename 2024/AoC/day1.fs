151198f2178494e6917a0f9df5c293579f482770
Nicholas <nicho.tjornelund@gmail.com>
2024-12-01 13:04:36 +0100

    day1

--- 2024/AoC/day1.fs
+++ 2024/AoC/day1.fs
@@ -1,4 +1,11 @@
 ﻿module day1
 
+open System.Collections;
+open System.Collections.Generic
 open System
+open System.IO;
+open utils
 
+let f = File.ReadAllLines("data/day1.txt")
+
+f |> List.map (fun s -> s.Split ' ')
