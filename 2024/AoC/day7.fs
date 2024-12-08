dd484a6b4998346fb1a32db3bf205a50c1bfacbb
Nicholas <nicho.tjornelund@gmail.com>
2024-12-08 15:36:37 +0100

    day 7 some sequence and common code

--- 2024/AoC/day7.fs
+++ 2024/AoC/day7.fs
@@ -11,39 +11,42 @@ let equations =
 
 type Token = 
     | NUM of int64
-    | CONCAT of Token * Token
+    | CONCAT
     | MULT
     | ADD
     | WILDCARD
 
+let interpreter (commands: Token list) = 
+    let reducer (acc:int64) (op_num: Token*Token) = 
+        match op_num with
+        | (MULT, NUM x) -> acc * x
+        | (CONCAT, NUM x) -> int64 $"{acc}{x}"
+        | (ADD, NUM x) -> acc + x
+        | _ -> acc
+
+    let init = match commands[0] with | NUM x -> int64 x
+    commands |> List.skip 1 |> List.pairwise |> List.fold reducer init
+
+let permutations selection length =
+    let rec inner acc n =
+        seq {
+            match n with
+            | 0 -> yield acc
+            | _ -> 
+                for t in selection do
+                    yield! inner (t :: acc) (n - 1)
+        }
+    inner [] length
+
+let merge (numbers: int64 array) (ops: Token list) = 
+    let m = ops |> List.mapi (fun i e -> [NUM numbers[i]; e] ) |> List.collect id
+    m @ [Array.last numbers |> NUM]
+
 let pt1() = 
-    
-    let interpreter (commands: Token list) = 
-        let reducer (acc:int64) op_num = 
-            match op_num with
-            | (MULT, NUM x) -> acc * x
-            | (ADD, NUM x) -> acc + x
-            | _ -> acc
-
-        let init = match commands[0] with | NUM x -> x
-        let a = commands |> List.skip 1 |> List.pairwise 
-        a |> List.fold reducer init
-
-    let permutations length =
-        let rec inner acc = 
-            function
-            | 0 -> [acc] 
-            | n -> [MULT; ADD] |> List.collect (fun t -> inner (t :: acc) (n - 1))
-        inner [] length
-        
-    let merge (numbers: int64 array) (ops: Token list) = 
-         let m = ops |> List.mapi (fun i e -> [NUM numbers[i]; e] ) |> List.collect id
-         m @ [Array.last numbers |> NUM]
-
     let correct (result:int64, numbers: int64 array) : bool = 
-        let ops = permutations (numbers.Length-1)
-        let eqs = ops |> List.map (fun op_list -> merge numbers op_list )
-        eqs |> List.exists (fun ops -> interpreter ops = result)
+        let ops = permutations [ADD;MULT] (numbers.Length-1)
+        let eqs = ops |> Seq.map (fun op_list -> merge numbers op_list )
+        eqs |> Seq.exists (fun ops -> interpreter ops = result)
 
     let result = equations 
                 |> Array.where correct
@@ -52,33 +55,11 @@ let pt1() =
     printfn "pt1 %i" result
 
 let pt2() = 
-    
-    let interpreter (commands: Token list) = 
-        let reducer (acc:int64) op_num = 
-            match op_num with
-            | (MULT, NUM x) -> acc * x
-            | (ADD, NUM x) -> acc + x
-            | _ -> acc
-
-        let init = match commands[0] with | NUM x -> x
-        let a = commands |> List.skip 1 |> List.pairwise 
-        a |> List.fold reducer init
-
-    let permutations length =
-        let rec inner acc = 
-            function
-            | 0 -> [acc] 
-            | n -> [MULT; ADD] |> List.collect (fun t -> inner (t :: acc) (n - 1))
-        inner [] length
-        
-    let merge (numbers: int64 array) (ops: Token list) = 
-         let m = ops |> List.mapi (fun i e -> [NUM numbers[i]; e] ) |> List.collect id
-         m @ [Array.last numbers |> NUM]
 
     let correct (result:int64, numbers: int64 array) : bool = 
-        let ops = permutations (numbers.Length-1)
-        let eqs = ops |> List.map (fun op_list -> merge numbers op_list )
-        eqs |> List.exists (fun ops -> interpreter ops = result)
+        let ops = permutations [ADD;MULT;CONCAT] (numbers.Length-1)
+        let eqs = ops |> Seq.map (fun op_list -> merge numbers op_list )
+        eqs |> Seq.exists (fun ops -> interpreter ops = result)
 
     let result = equations 
                 |> Array.where correct
