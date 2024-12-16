5fdff5f59ece2cf0b6942469da414bc3c97d97d7
Nicholas <nicho.tjornelund@gmail.com>
2024-12-16 20:41:56 +0100

    day 9 pt1

--- 2024/AoC/day9.fs
+++ 2024/AoC/day9.fs
@@ -4,66 +4,74 @@ open System.IO
 open System.Collections.Generic
 
 type Slot = 
-    | File of int*int*int //index, id, size
-    | Space of int*int//index, size
+    | File of int64*int64*int64 //index, id, size
+    | Space of int64*int64//index, size
 
-
-
-let rec indexAndDiscard idx = 
+let not_empty = 
     function
-    | [] -> []
-    | File(_,_,size)::xs when size = 0 -> (indexAndDiscard idx xs)
-    | Space(_,size)::xs when size = 0 -> (indexAndDiscard idx xs)
-    | File (_,id,size)::xs -> File(idx, id, size)::(indexAndDiscard (idx+size) xs)
-    | Space (_,size)::xs -> Space (idx,size)::(indexAndDiscard (idx+size) xs )
+    | File (_, _, size) when size > 0 -> true
+    | Space (_, size) when size > 0 -> true
+    | _ -> false
 
-let rec sorted (x::xs:Slot list) (y::ys:Slot list) idx =
+let index slots =
+    let accumulate (idx, acc) = 
+        function 
+        | File(_, id, size) -> (idx + size, File(idx, id, size) :: acc)
+        | Space(_, size) -> (idx + size, Space(idx, size) :: acc)
+
+    slots
+    |> List.fold accumulate (0, [])
+    |> snd
+    |> List.rev
+
+//Triangular numbers
+let squarenum n = n*(n+1L) / 2L
+
+let rec block_sum = 
+    function
+    | Space _ -> failwith ""
+    | File (i, id, size) -> 
+        let n = i+size-1L
+        let sumn = squarenum n
+        let sumi = squarenum (i-1L)
+        id * (sumn - sumi)
+
+let rec checksum (x::xs:Slot list) (y::ys:Slot list) idx sum =
     match x with
     | File (_,id,size) -> 
         match y with 
-        | File (_,yid,_) when id > yid -> []
-        | File (_,yid,_) when id = yid -> [y]
-        | _ -> x :: sorted xs (y::ys) (idx+size)
+        | File (_,yid,_) when id > yid -> sum
+        | File (_,yid,_) when id = yid -> sum + block_sum y
+        | _ -> checksum xs (y::ys) (idx+size) (sum+block_sum x)
+
     | Space(_, space_size) -> 
         match y with
-        | Space _ -> sorted (x::xs) (ys) idx
+        | Space _ -> checksum (x::xs) (ys) idx sum 
         | File (_, file_id, file_size) when space_size = file_size -> 
             //fit
-            File (idx, file_id, space_size) :: sorted xs ys (idx+file_size)
+            let file = File (idx, file_id, space_size)
+            checksum xs ys (idx+file_size) (sum+block_sum file)
 
         | File (_, file_id, file_size) when space_size > file_size  -> 
             //put file in space
             let space_remains = Space (idx+file_size, space_size - file_size)
-            File(idx, file_id, file_size) :: sorted (space_remains :: xs) ys (idx+file_size)
+            let file = File(idx, file_id, file_size)
+            checksum (space_remains :: xs) ys (idx+file_size) (sum+block_sum file)
 
         | File (file_index, file_id, file_size) when space_size < file_size  -> 
             //fill space up & split file
             let pt1 = File (idx, file_id, space_size)
             let pt2 = File (file_index, file_id, file_size - space_size)
-            pt1 :: sorted xs (pt2::ys) (idx+space_size)
-
-//Triangular numbers
-let squarenum n = n*(n+1) / 2
-
-let rec checksum = 
-    function
-    | [] -> 0
-    | Space _ :: xs -> failwith ""
-    | File (i, id, size) :: xs -> 
-        let n = i+size-1
-        let sumn = squarenum n
-        let sumi = squarenum (i-1)
-        let blocksum = id * (sumn - sumi)
-        blocksum + checksum xs
+            checksum xs (pt2::ys) (idx+space_size) (sum+block_sum pt1)
 
 let slots = File.ReadAllText("data/day9.txt").ToCharArray() 
             |> Array.map (fun c -> int c - int '0')
-            |> Array.mapi(fun i e -> if i % 2 = 0 then File (i, (i/2), e) else Space (i,e)) 
+            |> Array.mapi(fun i e -> if i % 2 = 0 then File (i, ((int64)i/2L), e) else Space (i,e)) 
+            |> Array.where not_empty
             |> List.ofArray
-            |> indexAndDiscard 0
+            |> index
 
-let slots_sorted = sorted slots (List.rev slots) 0
-let result = checksum slots_sorted
+let result = checksum slots (List.rev slots) 0 0
 printfn "%i" result
 
 
