3e229394de8203e9eec93ea47fb88302871f1035
Nicholas <nicho.tjornelund@gmail.com>
2024-12-21 12:36:41 +0100

    day 14 pt 1

--- 2024/AoC/day14.fs
+++ 2024/AoC/day14.fs
@@ -1,2 +1,29 @@
 ﻿module day14
 
+open Utils
+open System.IO
+open System.Collections.Generic
+
+let w = 101
+let h = 103
+
+let wraparound i n = (i % n + n) % n
+
+let translate (sec:int) = 
+    function 
+    | ((i,j), (x,y)) -> (wraparound (i+x*sec) w, wraparound (j+y*sec) h)
+    | _ -> failwith ""
+
+let f = File.ReadAllLines("data/day14.txt")
+        |> Array.map (TupleOf " ")
+        |> Array.map (fun (p,v) -> IntTupleOf "," p[2..], IntTupleOf "," v[2..])
+        |> Array.map (translate 100)
+        |> Array.filter (fun (i,j) -> i <> (w/2) && j <> (h/2))
+
+let quadrants = Dictionary<bool*bool, int>()
+for (i,j) in f do
+    let k = (i > (w/2), j > (h/2))
+    quadrants[k] <- quadrants.GetValueOrDefault(k,0) + 1
+
+let result = quadrants.Values |> Seq.reduce (*)
+printfn "pt1 %i" result
