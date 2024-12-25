14dd6ef972bc771f2535edc18487f08d22eae792
nicholas <nicho.tjornelund@gmail.com>
2024-12-25 15:44:34 +0100

    day 20 pt2

--- 2024/AoC/day20.fs
+++ 2024/AoC/day20.fs
@@ -25,6 +25,8 @@ let S = get_element 'S'
 let E = get_element 'E'
 
 let adj = [|(0,1);(0,-1);(1,0);(-1,0)|]
+let neighboors u = adj |> Array.map (fun dv -> dv++u)
+
 let dijkstra (source: int*int) =
 
     let queue = MinHeap<int*(int*int), int>(fst)
@@ -33,13 +35,13 @@ let dijkstra (source: int*int) =
 
     queue.Insert((0, source))
     dist[source] <- 0
+    prev[source] <- source
 
     while queue.Any() do
 
         let d, u = queue.RemoveMin()
 
-        for dv in adj do 
-            let v = u++dv
+        for v in neighboors u do 
             if item v <> '#' && dist[u]+1 < dist.GetValueOrDefault(v,Int32.MaxValue) then 
                 dist[v] <- dist[u]+1 
                 prev[v] <- u 
@@ -47,34 +49,58 @@ let dijkstra (source: int*int) =
         
     dist, prev
 
-let dist, path = dijkstra E
+let pt1() = 
+    let dist, path = dijkstra E
 
-let dist_SE = dist[S]
+    let rec cheat dist_origin u = 
+        function 
+        | 2 -> 
+            if item u <> '#' && dist[u] + 2 < dist_origin then 
+                [(dist_origin - dist[u] - 2)]
+            else 
+                []
+        | k -> 
+            [
+                for v in neighboors u do
+                    yield! cheat dist_origin v (k+1)
+            ]
+                
+    let result = 
+        path.Keys 
+        |> List.ofSeq 
+        |> List.filter (fun x -> x <> E)
+        |> List.collect (fun u -> cheat dist[u] u 0)
+        |> List.filter(fun x -> x >= 100)
+        |> List.length
 
-let neighboors u = adj |> Array.map (fun dv -> dv++u)
+    printfn "pt1 %i" result
 
-let rec cheat dist_origin u = 
-    function 
-    | 2 -> 
-        if item u <> '#' && dist[u] + 2 < dist_origin then 
-            [(dist_origin - dist[u] - 2)]
-        else 
-            []
-    | k -> 
-        [
-            for v in neighboors u do
-                yield! cheat dist_origin v (k+1)
-        ]
-            
-let all_cheats = 
-    path.Keys 
-    |> List.ofSeq 
-    |> List.filter (fun x -> x <> E)
-    |> List.collect (fun u -> cheat dist[u] u 0)
 
-let result = 
-    all_cheats 
-    |> List.filter(fun x -> x >= 100)
-    |> List.length
+let pt2 () = 
+    let manhattanDistance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+    let dist, prev = dijkstra E
 
-printfn "pt1 %i" result
+    (* let rec get_path u acc = 
+        match prev[u] with
+        | x when x = u -> acc
+        | x -> get_path x (x::acc) *)
+
+    let se1 = dist[S]
+    let se2 = manhattanDistance S E
+
+    //let path = get_path S []
+
+    let cheats = 
+        prev.Keys
+        |> Array.ofSeq 
+        |> Array.all_pairs
+        |> Array.filter (fun (a,b) -> manhattanDistance a b <= 20)
+        |> Array.collect (fun (a,b) -> [| (a,b); (b,a) |])
+        |> Array.where (fun (a,b) -> dist[b]+20 < dist[a])
+        |> Array.map (fun (a,b) -> dist[a]-dist[b]-manhattanDistance a b)
+        |> Array.where (fun saves -> saves >= 100)
+
+    printfn "pt2 %i" cheats.Length
+     
+//pt1()
+pt2() //1016066
