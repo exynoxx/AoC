d4ad9b353b3404a2170f190b16c12f20a677d679
Nicholas <nicho.tjornelund@gmail.com>
2025-01-02 12:40:40 +0100

    day 16 pt2 progress

--- 2024/AoC/day16.fs
+++ 2024/AoC/day16.fs
@@ -24,6 +24,13 @@ let get_element x =
 
 //        [|NORTH;EAST;SOUTH;WEST;|]
 let adj = [|(-1,0);(0,1);(1,0);(0,-1);|]
+
+let turn_price src tgt = 
+    match (src-tgt+4)%4 with
+    | 0 -> 0
+    | 2 -> 2000
+    | _ -> 1000
+
 let dijkstra s e =
 
     let queue = MinHeap<int*State, int>(fst)
@@ -35,33 +42,25 @@ let dijkstra s e =
     queue.Insert((0,{Orientation=1; Pos = s}))
 
     while queue.Any() do
-        let cost, {Orientation=ori;Pos=u} = queue.RemoveMin()
+        let cost, {Orientation=ori_u;Pos=u} = queue.RemoveMin()
 
         if u <> e then
 
             for i in 0..3 do
                 let v = u++adj[i]
 
-                let price = 
-                    match (ori-i+4)%4 with
-                    | 0 -> 1
-                    | 2 -> 2001
-                    | 1 | 3 -> 1001
-
-                if item v <> '#' && price <> 2001 then
-
-                    if v = (7,15) then
-                         ()
-
+                let price = (turn_price ori_u i) + 1
+                if item v <> '#' then
                     let dist_v = dist.GetValueOrDefault(v, Int32.MaxValue)
 
-                    if dist[u]+price-1000 = dist_v then
-                        prev[v] <- u::prev[v]
-
-                    if dist[u]+price < dist_v then
+                    if dist[u]+price < dist_v then 
                         dist[v] <- dist[u]+price
                         prev[v] <- [u]
+
                         queue.Insert((dist[v],{Orientation=i;Pos=v}))
+
+                    elif v <> e && dist[u] + 1 - 1000 = dist_v then 
+                        prev[v] <- u::prev[v]
 
     dist, prev
     
@@ -76,20 +75,51 @@ let pt1() =
 
 let pt2 () =
     let positions = HashSet<int*int>()
+    let falsee = HashSet<int*int>()
 
-    let rec Backwards u = 
-        positions.Add u |> ignore
+    let rec Backwards acc i u = 
         match prev[u] with
         | [] -> ()
-        | [x] when x = S -> ()
-        | [x] -> 
-            Backwards x
-        | xs -> 
-            for x in xs do 
-                Backwards x
+        | _ when u = S -> 
+            if abs(i - (dist[E] % 1000)) <= 0 then 
+                for x in acc do positions.Add x
 
-    Backwards E
+        | [x] -> Backwards (u::acc) (i+1) x
+
+        | xs -> 
+            //1000 steps == 1 turn
+            (*if acc.Length > 0 then 
+                let last_intersection = List.last acc
+                let len = acc.Length
+                let correct_len =  (dist[last_intersection] - dist[u]) % 1000
+
+                if abs(len - correct_len) <= 1 then 
+                    (u::acc) |> List.map positions.Add |> ignore
+                else
+                    acc |> List.map falsee.Add |> ignore
+
+                for x in xs do 
+                    Backwards [u] 0 x
+            else    
+                for x in xs do 
+                   Backwards (u::acc) 0 x *)
+            for x in xs do 
+                Backwards (u::acc) (i+1) x
+
+    Backwards [E] 0 E
+
+    for (i,j) in positions do 
+        G[i][j] <- 'O'
+
+    for (i,j) in falsee do 
+        G[i][j] <- 'X'
+
+    for row in G do
+        row |> Array.iter (printf "%c")
+        printfn ""
+
     printfn "pt2 %i" positions.Count
+    //552 too high
 
 pt1()
 pt2()
